34d827a9292aa33951e818685076df3de8a0014f
Brian Keating <brikeats@gmail.com>
2017-09-23 14:25:41 -0500

    Empty website & blog

---
+++ content/test_post2.md
@@ -0,0 +1,16 @@
+Title: Testes 2
+Date: 2017-09-24 12:50
+Modified: 2017-09-24 12:50
+Category: 
+Tags: pelican, test
+Slug: post2
+Author: Brian Keating
+Summary: Title for post 2 when the post is collapsed
+
+
+# Post 2
+
+* list item
+* list item2
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
--- pelicanconf.py
+++ pelicanconf.py
@@ -12,6 +12,21 @@ TIMEZONE = 'America/Chicago'
 
 DEFAULT_LANG = u'en'
 
+# # static paths will be copied without parsing their contents
+STATIC_PATHS = ['images']  # 'extra/robots.txt'
+
+INDEX_SAVE_AS = 'blog.html'
+DISPLAY_PAGES_ON_MENU = False
+DISPLAY_CATEGORIES_ON_MENU = False
+
+# the links here should match the slugs in the files
+MENUITEMS = (
+    ('HOME', '/'),
+    ('ABOUT', '/pages/about-brian.html'),  
+    ('BLOG', '/blog.html')
+)
+
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
@@ -19,15 +34,15 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+# # Blogroll
+# LINKS = (('Pelican', 'http://getpelican.com/'),
+#          ('Python.org', 'http://python.org/'),
+#          ('Jinja2', 'http://jinja.pocoo.org/'),
+#          ('You can modify those links in your config file', '#'),)
 
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+# # Social widget
+# SOCIAL = (('You can add links in your config file', '#'),
+#           ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
 
